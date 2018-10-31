254b051cc845e721aad8d10e5d7877aed6dc87a4
Jon.Xia <xiajia@vip.qq.com>
2018-10-31 17:38:44 +0800

    去掉不必要的路由，调整多语言显示， 增加默认首页

--- src/components/SelectLang/index.js
+++ src/components/SelectLang/index.js
@@ -15,28 +15,13 @@ export default class SelectLang extends PureComponent {
     const langMenu = (
       <Menu className={styles.menu} selectedKeys={[selectedLang]} onClick={this.changLang}>
         <Menu.Item key="zh-CN">
-          <span role="img" aria-label="简体中文">
-            🇨🇳
-          </span>{' '}
           简体中文
         </Menu.Item>
         <Menu.Item key="zh-TW">
-          <span role="img" aria-label="繁体中文">
-            🇭🇰
-          </span>{' '}
           繁体中文
         </Menu.Item>
         <Menu.Item key="en-US">
-          <span role="img" aria-label="English">
-            🇬🇧
-          </span>{' '}
           English
-        </Menu.Item>
-        <Menu.Item key="pt-BR">
-          <span role="img" aria-label="Português">
-            🇵🇹
-          </span>{' '}
-          Português
         </Menu.Item>
       </Menu>
     );
